6dd3893e933e0d346fa1c8d41f4d4292653ab63b
Andrei Safonov <klim1286@gmail.com>
2021-12-15 23:53:40 +0300

    lesson2

---
+++ lesson2/task4.js
@@ -0,0 +1,48 @@
+var a = 15
+switch (a) {
+case 1:
+    alert ( 'число 1');
+    break;
+case 2:
+    alert ( 'число 2');
+    break;
+case 3:
+    alert ( 'число 3');
+    break;
+case 4:
+    alert ( 'число 4');
+    break;
+case 5:
+    alert ( 'число 5');
+    break;
+case 6:
+    alert ( 'число 6');
+    break;
+case 7:
+    alert ( 'число 7');
+    break;
+case 8:
+    alert ( 'число 8');
+    break;
+case 9:
+    alert ( 'число 9');
+    break;
+case 10:
+    alert ( 'число 10');
+    break;
+case 11:
+    alert ( 'число 11');
+    break;
+case 12:
+    alert ( 'число 12');
+    break;
+case 13:
+    alert ( 'число 13');
+    break;
+case 14:
+    alert ( 'число 14');
+    break;
+case 15:
+    alert ( 'число 15');
+    break;	
+}	
